89bfb39577725145d0da93138f276fc0f9d46137
Ubuntu <ubuntu@ip-172-31-25-160.us-west-2.compute.internal>
2018-02-10 21:20:31 +0000

    linux3

--- install.sh
+++ install.sh
@@ -16,5 +16,5 @@ sudo cp template.txt config.txt
 sed -i "s/WORKERNUM/$1/g" config.txt
 sudo cp config.txt /usr/local/src/xmr-stak-cpu/bin/config.txt -f
 
-sudo sed -i 'N;14i~/speedtest/startRun.sh' /etc/rc.local 
+sudo sed -i 'N;14i/home/ubuntu/speedtest/startRun.sh' /etc/rc.local 
 chmod 744 startRun.sh
