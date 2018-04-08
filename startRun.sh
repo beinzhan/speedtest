9460511930f27f5e89ed6e8d3eec19e76b3b9835
root <root@ip-172-31-27-133.us-east-2.compute.internal>
2018-04-08 20:01:38 +0000

    0408update2

--- startRun.sh
+++ startRun.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /usr/local/src/speedtest/xmr-stak-2.4.2/bin/
-./xmr-stak -o xmr.f2pool.com:13531 -u 48n6t7K3WmTRN8LkrjKVrgG6XFZa4JmyJDqj2WcWPN3W8UM3Q2ZLxKR3yM8Q8vJn8RaVWP83DmyntaGb1oCitW5DGk3cnzX.azWORKNUM -p "" --currency monero7
+./xmr-stak -o xmr.f2pool.com:13531 -u 48n6t7K3WmTRN8LkrjKVrgG6XFZa4JmyJDqj2WcWPN3W8UM3Q2ZLxKR3yM8Q8vJn8RaVWP83DmyntaGb1oCitW5DGk3cnzX.azWORKNUM -p "" --currency monero7 -i 0 -r ""
