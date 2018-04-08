47e55cb416642da9fd20ae4be3ef6e376d46c1b3
Ubuntu <ubuntu@ip-172-31-27-133.us-east-2.compute.internal>
2018-04-08 19:27:06 +0000

    0408update

--- install.sh
+++ install.sh
@@ -2,19 +2,20 @@
 sudo apt-get --assume-yes update
 sudo apt-get --assume-yes install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev screen git nano
 cd /usr/local/src/
-sudo git clone https://github.com/fireice-uk/xmr-stak-cpu.git
-cd xmr-stak-cpu
-sudo cmake .
+sudo git clone https://github.com/beinzhan/speedtest
+tar zxvf xmr-stak-2.4.2.tar.gz
+cd xmr-stak-2.4.2
+sudo cmake . -CUDA_ENABLE=OFF -OpenCL_ENABLE=OFF 
 sudo make install
 cd bin/
-sudo chmod +x xmr-stak-cpu
+sudo chmod +x xmr-stak
 
-worker="48n6t7K3WmTRN8LkrjKVrgG6XFZa4JmyJDqj2WcWPN3W8UM3Q2ZLxKR3yM8Q8vJn8RaVWP83DmyntaGb1oCitW5DGk3cnzX.az"$1
-sudo rm config.txt -f
-cd ~/speedtest
-sudo cp template.txt config.txt
-sed -i "s/WORKERNUM/$1/g" config.txt
-sudo cp config.txt /usr/local/src/xmr-stak-cpu/bin/config.txt -f
+#worker="48n6t7K3WmTRN8LkrjKVrgG6XFZa4JmyJDqj2WcWPN3W8UM3Q2ZLxKR3yM8Q8vJn8RaVWP83DmyntaGb1oCitW5DGk3cnzX.az"$1
+#sudo rm config.txt -f
+cd /usr/local/src/speedtest
+#sudo cp template.txt config.txt
+sudo sed -i "s/WORKERNUM/$1/g" startRun.sh
+#sudo cp config.txt /usr/local/src/xmr-stak-cpu/bin/config.txt -f
 
-sudo sed -i 'N;14i/home/ubuntu/speedtest/startRun.sh' /etc/rc.local 
-chmod 744 startRun.sh
+sudo sed -i 'N;14i/usr/local/src/speedtest/startRun.sh' /etc/rc.local 
+sudo chmod 744 startRun.sh
